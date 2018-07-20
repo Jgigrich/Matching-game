f121743cd2ca795bf2ac32b7e89d02e2b04d68e1
Jgigrich <github@GreenSeaProductions.com>
2018-07-19 21:36:47 -0500

    Made the design responsive.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ let numInSets = 2;
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -12,7 +11,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
